3087d908555f4d75e36ed3ddb04b809efcd36af3
Abhinav <abhinavkumr77@gmail.com>
2023-07-30 22:30:41 +0530

    new controllers and packages added

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -6,7 +6,7 @@ public class Ingredient {
 
 	private final String id;
 	private final String name;
-	private final String type;
+	private final Type type;
 	
 	public enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
